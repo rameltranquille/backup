f7aad2f8f106e423a4a157ece925b18e3cb084bc
rameltranquille <tranquilleramel@gmail.com>
2023-01-09 14:02:57 -0500

    Updated on

--- backup.sh
+++ backup.sh
@@ -7,15 +7,15 @@ ALT_CONFIGS="$CF/oh-my-zsh $HOME/.vim/vimrc-extra $HOME/.vim/vimrc $HOME/.taskrc
 for folder in $CONFIGS
 do
     echo "syncing $folder...\n"
-    rsync -aiv "$CF$folder" "$CF/backup"
-    rsync -aiv "$CF$folder" "$HOME/Dropbox/backups"
+    rsync -aiv "$CF/$folder" "$CF/backup"
+    rsync -aiv "$CF/$folder" "$HOME/Dropbox/backups"
 done
 
 for alt in $ALT_CONFIGS
 do
     echo "syncing $alt...\n"
-    rsync -aiv "$CF$alt" "$CF/backup"
-    rsync -aiv "$CF$alt" "$HOME/Dropbox/backups"
+    rsync -aiv "$CF/$alt" "$CF/backup"
+    rsync -aiv "$CF/$alt" "$HOME/Dropbox/backups"
 done
 
 mv $CF/backup/custom $CF/backup/oh-my-zsh-custom
